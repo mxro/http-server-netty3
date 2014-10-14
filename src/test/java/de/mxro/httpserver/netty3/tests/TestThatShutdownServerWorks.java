9ae4b57cad56872c876afc363ab05a0085c6ad14
mxro <mxro@nowhere.com>
2014-10-15 10:40:38 +1100

    Reorganizing Netty and http-server projects. Assembling http-server implementation using Netty 3 Autocommit Wed Oct 15 10:40:38 EST 2014

---
+++ src/test/java/de/mxro/httpserver/netty3/tests/TestThatShutdownServerWorks.java
@@ -0,0 +1,10 @@
+package de.mxro.httpserver.netty3.tests;
+
+import org.junit.Test;
+
+@SuppressWarnings("all")
+public class TestThatShutdownServerWorks {
+  @Test
+  public void test() {
+  }
+}
---
+++ src/test/xtend/de/mxro/httpserver/netty3/tests/TestThatShutdownServerWorks.xtend
@@ -0,0 +1,12 @@
+package de.mxro.httpserver.netty3.tests
+
+import org.junit.Test
+
+class TestThatShutdownServerWorks {
+	
+	@Test
+	def void test() {
+		
+	}
+	
+}
