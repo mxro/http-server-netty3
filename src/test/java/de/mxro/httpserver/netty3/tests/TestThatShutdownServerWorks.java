71075f7ac408bc42990a5e22928570dcdec92483
mxro <mxro@nowhere.com>
2014-10-15 10:46:38 +1100

    Reorganizing Netty and http-server projects. Assembling http-server implementation using Netty 3 Autocommit Wed Oct 15 10:46:38 EST 2014

--- src/test/java/de/mxro/httpserver/netty3/tests/TestThatShutdownServerWorks.java
+++ src/test/java/de/mxro/httpserver/netty3/tests/TestThatShutdownServerWorks.java
@@ -8,9 +8,7 @@ public class TestThatShutdownServerWorks {
   public void test() {
     throw new Error("Unresolved compilation problems:"
       + "\nno viable alternative at input \')\'"
-      + "\nno viable alternative at input \')\'"
       + "\nInvalid number of arguments. The method startShutdownServer(int, String, ServerComponent, ValueCallback<Netty3ServerComponent>) is not applicable for the arguments (int,String)"
-      + "\nType mismatch: cannot convert from HttpService to Netty3ServerConfiguration"
-      + "\nType mismatch: cannot convert from int to ValueCallback<Netty3ServerComponent>");
+      + "\nType mismatch: cannot convert from SimpleCallback to ValueCallback<Netty3ServerComponent>");
   }
 }
--- src/test/xtend/de/mxro/httpserver/netty3/tests/TestThatShutdownServerWorks.xtend
+++ src/test/xtend/de/mxro/httpserver/netty3/tests/TestThatShutdownServerWorks.xtend
@@ -1,5 +1,6 @@
 package de.mxro.httpserver.netty3.tests
 
+import de.mxro.async.Async
 import de.mxro.async.jre.AsyncJre
 import de.mxro.httpserver.netty3.Netty3Server
 import de.mxro.httpserver.services.Services
@@ -11,7 +12,7 @@ class TestThatShutdownServerWorks {
 	def void test() {
 		
 		AsyncJre.waitFor([cb |
-			Netty3Server.start(Services.echoService(), 12322, );
+			Netty3Server.start(Services.echoService(), 12322, Async.wrap(cb));
 		])
 		
 		
