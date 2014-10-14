fbc69e14bb03ca5382b53d23bb7c104948fdb77b
mxro <mxro@nowhere.com>
2014-10-15 10:49:38 +1100

    Reorganizing Netty and http-server projects. Assembling http-server implementation using Netty 3 Autocommit Wed Oct 15 10:49:38 EST 2014

--- src/test/java/de/mxro/httpserver/netty3/tests/TestThatShutdownServerWorks.java
+++ src/test/java/de/mxro/httpserver/netty3/tests/TestThatShutdownServerWorks.java
@@ -7,24 +7,31 @@ import de.mxro.httpserver.HttpService;
 import de.mxro.httpserver.netty3.Netty3Server;
 import de.mxro.httpserver.netty3.Netty3ServerComponent;
 import de.mxro.httpserver.services.Services;
+import java.net.URL;
+import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.junit.Test;
 
 @SuppressWarnings("all")
 public class TestThatShutdownServerWorks {
   @Test
   public void test() {
-    final Deferred<Netty3ServerComponent> _function = new Deferred<Netty3ServerComponent>() {
-      public void get(final ValueCallback<Netty3ServerComponent> cb) {
-        HttpService _echoService = Services.echoService();
-        Netty3Server.start(_echoService, 12322, cb);
-      }
-    };
-    final Netty3ServerComponent server = AsyncJre.<Netty3ServerComponent>waitFor(_function);
-    final Deferred<Netty3ServerComponent> _function_1 = new Deferred<Netty3ServerComponent>() {
-      public void get(final ValueCallback<Netty3ServerComponent> cb) {
-        Netty3Server.startShutdownServer(12321, "mysecret", server, cb);
-      }
-    };
-    final Netty3ServerComponent shutdown = AsyncJre.<Netty3ServerComponent>waitFor(_function_1);
+    try {
+      final Deferred<Netty3ServerComponent> _function = new Deferred<Netty3ServerComponent>() {
+        public void get(final ValueCallback<Netty3ServerComponent> cb) {
+          HttpService _echoService = Services.echoService();
+          Netty3Server.start(_echoService, 12322, cb);
+        }
+      };
+      final Netty3ServerComponent server = AsyncJre.<Netty3ServerComponent>waitFor(_function);
+      final Deferred<Netty3ServerComponent> _function_1 = new Deferred<Netty3ServerComponent>() {
+        public void get(final ValueCallback<Netty3ServerComponent> cb) {
+          Netty3Server.startShutdownServer(12321, "mysecret", server, cb);
+        }
+      };
+      final Netty3ServerComponent shutdownServer = AsyncJre.<Netty3ServerComponent>waitFor(_function_1);
+      new URL("http://localhost:12321/mysecret");
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
   }
 }
--- src/test/xtend/de/mxro/httpserver/netty3/tests/TestThatShutdownServerWorks.xtend
+++ src/test/xtend/de/mxro/httpserver/netty3/tests/TestThatShutdownServerWorks.xtend
@@ -1,10 +1,10 @@
 package de.mxro.httpserver.netty3.tests
 
-import de.mxro.async.Async
 import de.mxro.async.jre.AsyncJre
 import de.mxro.httpserver.netty3.Netty3Server
 import de.mxro.httpserver.services.Services
 import org.junit.Test
+import java.net.URL
 
 class TestThatShutdownServerWorks {
 	
@@ -15,12 +15,12 @@ class TestThatShutdownServerWorks {
 			Netty3Server.start(Services.echoService(), 12322, cb);
 		])
 		
-		val shutdown = AsyncJre.waitFor([cb |
+		val shutdownServer = AsyncJre.waitFor([cb |
 			Netty3Server.startShutdownServer(12321, "mysecret", server, cb);
 		])
 		
 		
-		
+		new URL("http://localhost:12321/mysecret");
 		
 		
 	}
