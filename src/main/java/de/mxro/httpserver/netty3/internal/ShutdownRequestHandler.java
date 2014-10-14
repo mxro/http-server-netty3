bb4012af9c6ab0717465a8f5dc11e562ab0ff614
mxro <mxro@nowhere.com>
2014-10-15 09:12:21 +1100

    Reorganizing Netty and http-server projects. Assembling http-server implementation using Netty 3 Autocommit Wed Oct 15 09:12:21 EST 2014

--- src/main/java/de/mxro/httpserver/netty3/internal/ShutdownRequestHandler.java
+++ src/main/java/de/mxro/httpserver/netty3/internal/ShutdownRequestHandler.java
@@ -9,130 +9,121 @@ import java.io.UnsupportedEncodingException;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import nx.server.netty.NxServerNetty;
-
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
 import org.jboss.netty.handler.codec.http.HttpRequest;
 
 import de.mxro.httpserver.netty3.Netty3Server;
+import de.mxro.httpserver.netty3.NxServerNetty;
 import de.mxro.server.ServerComponent;
 import de.mxro.service.callbacks.ShutdownCallback;
 
 public class ShutdownRequestHandler extends SimpleChannelUpstreamHandler {
 
-	private final String secret;
-	final ServerComponent shutdownOperations;
-	Netty3Server thisServer;
+    private final String secret;
+    final ServerComponent shutdownOperations;
+    Netty3Server thisServer;
 
-	@Override
-	public void messageReceived(final ChannelHandlerContext ctx,
-			final MessageEvent e) throws Exception {
+    @Override
+    public void messageReceived(final ChannelHandlerContext ctx, final MessageEvent e) throws Exception {
 
-		final Thread t = new Thread() {
+        final Thread t = new Thread() {
 
-			@Override
-			public void run() {
+            @Override
+            public void run() {
 
-				// System.out.println("doing shutdown ...");
-				final HttpRequest request = (HttpRequest) e.getMessage();
+                // System.out.println("doing shutdown ...");
+                final HttpRequest request = (HttpRequest) e.getMessage();
 
-				final String requestUri = request.getUri();
+                final String requestUri = request.getUri();
 
-				if (!requestUri.replace("/", "").equals(secret)) {
+                if (!requestUri.replace("/", "").equals(secret)) {
 
-					try {
-						NxServerNetty
-								.sendHttpResponse(
-										e,
-										"Access denied. You must supply the master secret for this server as part of the url, eg: http://myserver.com:8900/[your secret]"
-												.getBytes("UTF-8"),
-										403, "text/plain");
+                    try {
+                        NxServerNetty
+                                .sendHttpResponse(
+                                        e,
+                                        "Access denied. You must supply the master secret for this server as part of the url, eg: http://myserver.com:8900/[your secret]"
+                                                .getBytes("UTF-8"), 403, "text/plain");
 
-						return;
-					} catch (final Throwable t) {
-						throw new RuntimeException(t);
-					}
+                        return;
+                    } catch (final Throwable t) {
+                        throw new RuntimeException(t);
+                    }
 
-				}
+                }
 
-				shutdownOperations.stop(new ShutdownCallback() {
+                shutdownOperations.stop(new ShutdownCallback() {
 
-					@Override
-					public void onSuccess() {
+                    @Override
+                    public void onSuccess() {
 
-						try {
-							// new Exception("Shutdown successful")
-							// .printStackTrace();
-							NxServerNetty.sendHttpSuccess(e,
-									"Shutdown successful.".getBytes("UTF-8"),
-									"text/plain");
+                        try {
+                            // new Exception("Shutdown successful")
+                            // .printStackTrace();
+                            NxServerNetty.sendHttpSuccess(e, "Shutdown successful.".getBytes("UTF-8"), "text/plain");
 
-							final TimerTask stopShutdownServer = new TimerTask() {
+                            final TimerTask stopShutdownServer = new TimerTask() {
 
-								@Override
-								public void run() {
-									assert thisServer != null : "setThisServer() must be specified for this shutdown server.";
+                                @Override
+                                public void run() {
+                                    assert thisServer != null : "setThisServer() must be specified for this shutdown server.";
 
-									thisServer.stop(new ShutdownCallback() {
+                                    thisServer.stop(new ShutdownCallback() {
 
-										@Override
-										public void onSuccess() {
-											// all ok
+                                        @Override
+                                        public void onSuccess() {
+                                            // all ok
 
-										}
+                                        }
 
-										@Override
-										public void onFailure(final Throwable t) {
-											throw new RuntimeException(t);
-										}
-									});
-								}
+                                        @Override
+                                        public void onFailure(final Throwable t) {
+                                            throw new RuntimeException(t);
+                                        }
+                                    });
+                                }
 
-							};
+                            };
 
-							new Timer().schedule(stopShutdownServer, 150);
+                            new Timer().schedule(stopShutdownServer, 150);
 
-						} catch (final UnsupportedEncodingException e1) {
-							throw new RuntimeException(e1);
-						}
+                        } catch (final UnsupportedEncodingException e1) {
+                            throw new RuntimeException(e1);
+                        }
 
-					}
+                    }
 
-					@Override
-					public void onFailure(final Throwable t) {
+                    @Override
+                    public void onFailure(final Throwable t) {
 
-						System.out
-								.println("Failure while shutting down server: "
-										+ t.getMessage());
-						t.printStackTrace();
+                        System.out.println("Failure while shutting down server: " + t.getMessage());
+                        t.printStackTrace();
 
-						NxServerNetty.sendHttpSuccess(e, t.getMessage()
-								.getBytes(), "text/plain");
+                        NxServerNetty.sendHttpSuccess(e, t.getMessage().getBytes(), "text/plain");
 
-					}
+                    }
 
-				});
-			}
+                });
+            }
 
-		};
+        };
 
-		t.start();
+        t.start();
 
-	}
+    }
 
-	public void setThisServer(final Netty3Server server) {
-		this.thisServer = server;
-	}
+    public void setThisServer(final Netty3Server server) {
+        this.thisServer = server;
+    }
 
-	public ShutdownRequestHandler(final String secret,
-			final ServerComponent shutdownOperations) {
-		super();
+    public ShutdownRequestHandler(final String secret, final ServerComponent shutdownOperations) {
+        super();
 
-		this.shutdownOperations = shutdownOperations;
-		this.secret = secret;
+        this.shutdownOperations = shutdownOperations;
+        this.secret = secret;
 
-	}
+    }
 
 }
--- src/main/java/de/mxro/httpserver/netty3/internal/ShutdownServerFactory.java
+++ src/main/java/de/mxro/httpserver/netty3/internal/ShutdownServerFactory.java
@@ -11,8 +11,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 
-import mx.jreutils.MxJREUtils;
-
 import org.jboss.netty.bootstrap.ServerBootstrap;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
@@ -30,107 +28,99 @@ import de.mxro.server.ServerComponent;
  */
 public class ShutdownServerFactory {
 
-	private final static boolean ENABLE_DEBUG = false;
+    private final static boolean ENABLE_DEBUG = false;
 
-	public static Netty3Server startNettyShutdownServer(final int port,
-			final String secret, final ServerComponent shutdownOperations) {
-		final ThreadFactory threadFactory = new ThreadFactory() {
+    public static Netty3Server startNettyShutdownServer(final int port, final String secret,
+            final ServerComponent shutdownOperations) {
+        final ThreadFactory threadFactory = new ThreadFactory() {
 
-			@Override
-			public Thread newThread(final Runnable arg0) {
+            @Override
+            public Thread newThread(final Runnable arg0) {
 
-				return new Thread(arg0, "shutdown-server-"
-						+ new Random().nextInt(2000));
-			}
-		};
-		final ExecutorService newFixedThreadPool = Executors.newCachedThreadPool(threadFactory);
+                return new Thread(arg0, "shutdown-server-" + new Random().nextInt(2000));
+            }
+        };
+        final ExecutorService newFixedThreadPool = Executors.newCachedThreadPool(threadFactory);
 
-		final ServerBootstrap bootstrap = new ServerBootstrap(
-				new NioServerSocketChannelFactory(newFixedThreadPool,
-						newFixedThreadPool));
+        final ServerBootstrap bootstrap = new ServerBootstrap(new NioServerSocketChannelFactory(newFixedThreadPool,
+                newFixedThreadPool));
 
-		final ShutdownRequestHandler shutdownRequestHandler = new ShutdownRequestHandler(
-				secret, shutdownOperations);
+        final ShutdownRequestHandler shutdownRequestHandler = new ShutdownRequestHandler(secret, shutdownOperations);
 
-		bootstrap.setPipelineFactory(new ShutdownServerPipelineFactory(
-				shutdownRequestHandler));
+        bootstrap.setPipelineFactory(new ShutdownServerPipelineFactory(shutdownRequestHandler));
 
-		if (ENABLE_DEBUG) {
-			System.out.println(ShutdownServerFactory.class
-					+ ": Binding shutdown server on port: " + port);
-		}
+        if (ENABLE_DEBUG) {
+            System.out.println(ShutdownServerFactory.class + ": Binding shutdown server on port: " + port);
+        }
 
-		if (!MxJREUtils.portAvailable(port)) {
-			throw new IllegalStateException(
-					"Cannot start shutdown server on port: " + port + "\n"
-							+ "  The port is already in use.");
-		}
+        if (!MxJREUtils.portAvailable(port)) {
+            throw new IllegalStateException("Cannot start shutdown server on port: " + port + "\n"
+                    + "  The port is already in use.");
+        }
 
-		// Bind and start to accept incoming connections.
+        // Bind and start to accept incoming connections.
 
-		final Channel server = bootstrap.bind(new InetSocketAddress(port));
+        final Channel server = bootstrap.bind(new InetSocketAddress(port));
 
-		if (ENABLE_DEBUG) {
-			System.out.println(ShutdownServerFactory.class
-					+ ": Shutdown server started on port: " + port);
-		}
+        if (ENABLE_DEBUG) {
+            System.out.println(ShutdownServerFactory.class + ": Shutdown server started on port: " + port);
+        }
 
-		final Netty3Server nettyServer = new Netty3Server() {
+        final Netty3Server nettyServer = new Netty3Server() {
 
-			@Override
-			public int getPort() {
+            @Override
+            public int getPort() {
 
-				return port;
-			}
+                return port;
+            }
 
-			@Override
-			public Channel getChannel() {
+            @Override
+            public Channel getChannel() {
 
-				return server;
-			}
+                return server;
+            }
 
-			@Override
-			public void stop(final SimpleCallback callback) {
-				getChannel().close().awaitUninterruptibly(3000);
-				bootstrap.releaseExternalResources();
-				callback.onSuccess();
+            @Override
+            public void stop(final SimpleCallback callback) {
+                getChannel().close().awaitUninterruptibly(3000);
+                bootstrap.releaseExternalResources();
+                callback.onSuccess();
 
-			}
+            }
 
-			@Override
-			public void start(final SimpleCallback callback) {
-				throw new RuntimeException("Operation not supported!");
-			}
+            @Override
+            public void start(final SimpleCallback callback) {
+                throw new RuntimeException("Operation not supported!");
+            }
 
-			@Override
-			public void injectConfiguration(final ComponentConfiguration conf) {
-				throw new RuntimeException("Operation not supported!");
-			}
+            @Override
+            public void injectConfiguration(final ComponentConfiguration conf) {
+                throw new RuntimeException("Operation not supported!");
+            }
 
-			@Override
-			public void injectContext(final ComponentContext context) {
-				throw new RuntimeException("Operation not supported!");
-			}
+            @Override
+            public void injectContext(final ComponentContext context) {
+                throw new RuntimeException("Operation not supported!");
+            }
 
-			@Override
-			public ComponentConfiguration getConfiguration() {
-				throw new RuntimeException("Operation not supported!");
-			}
+            @Override
+            public ComponentConfiguration getConfiguration() {
+                throw new RuntimeException("Operation not supported!");
+            }
 
-			@Override
-			public void destroy(final SimpleCallback callback) {
-				getChannel().close().awaitUninterruptibly(3000);
-				bootstrap.releaseExternalResources();
-				callback.onSuccess();
+            @Override
+            public void destroy(final SimpleCallback callback) {
+                getChannel().close().awaitUninterruptibly(3000);
+                bootstrap.releaseExternalResources();
+                callback.onSuccess();
 
-			}
-		};
+            }
+        };
 
-		shutdownRequestHandler.setThisServer(nettyServer);
+        shutdownRequestHandler.setThisServer(nettyServer);
 
-		return new InternalShutdownServer(port, server, shutdownRequestHandler,
-				bootstrap);
+        return new InternalShutdownServer(port, server, shutdownRequestHandler, bootstrap);
 
-	}
+    }
 
 }
