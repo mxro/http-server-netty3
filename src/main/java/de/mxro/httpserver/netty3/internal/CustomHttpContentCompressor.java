c41891fee85e231ab498fcb3cda31c889ccc2a14
mxro <mxro@nowhere.com>
2014-10-15 14:25:25 +1100

    Assembling http-server implementation for Netty 4 Autocommit Wed Oct 15 14:25:25 EST 2014

--- src/main/java/de/mxro/httpserver/netty3/internal/CustomHttpContentCompressor.java
+++ src/main/java/de/mxro/httpserver/netty3/internal/CustomHttpContentCompressor.java
@@ -3,6 +3,7 @@ package de.mxro.httpserver.netty3.internal;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.handler.codec.http.HttpContentCompressor;
+import org.jboss.netty.handler.codec.http.HttpHeaders;
 import org.jboss.netty.handler.codec.http.HttpMessage;
 
 public class CustomHttpContentCompressor extends HttpContentCompressor {
@@ -14,7 +15,7 @@ public class CustomHttpContentCompressor extends HttpContentCompressor {
         final HttpMessage httpMessage = (HttpMessage) e.getMessage();
         final int length = httpMessage.getContent().array().length;
 
-        httpMessage.addHeader(arg0, arg1);
+        httpMessage.addHeader(HttpHeaders.Names.CONTENT_LENGTH, length);
 
     }
 
