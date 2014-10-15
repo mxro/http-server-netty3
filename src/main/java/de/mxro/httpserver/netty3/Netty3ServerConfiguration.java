f8a75406a3108725fbefa93f543b94752abe9ac2
mxro <mxro@nowhere.com>
2014-10-16 07:50:56 +1100

    Clearing up project Autocommit Thu Oct 16 07:50:56 EST 2014

--- src/main/java/de/mxro/httpserver/netty3/Netty3ServerConfiguration.java
+++ src/main/java/de/mxro/httpserver/netty3/Netty3ServerConfiguration.java
@@ -1,12 +1,7 @@
 package de.mxro.httpserver.netty3;
 
 import de.mxro.httpserver.HttpsServerConfiguration;
-import de.mxro.sslutils.SslKeyStoreData;
 
 public abstract class Netty3ServerConfiguration implements HttpsServerConfiguration {
-
-    public abstract boolean getUseSsl();
-
-    public abstract SslKeyStoreData getSslKeyStore();
 
 }
