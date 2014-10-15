8f9acd49df7f7ae1a97ac1e58aaf629fda4abe2a
mxro <mxro@nowhere.com>
2014-10-15 12:10:25 +1100

    Assembling http-server implementation for Netty 4 Autocommit Wed Oct 15 12:10:25 EST 2014

--- src/main/java/de/mxro/httpserver/netty3/Netty3ServerConfiguration.java
+++ src/main/java/de/mxro/httpserver/netty3/Netty3ServerConfiguration.java
@@ -5,12 +5,11 @@ import de.mxro.sslutils.SslKeyStoreData;
 
 public abstract class Netty3ServerConfiguration {
 
-	public abstract int getPort();
+    public abstract int getPort();
 
-	public abstract boolean getUseSsl();
+    public abstract boolean getUseSsl();
 
-	public abstract SslKeyStoreData getSslKeyStore();
+    public abstract SslKeyStoreData getSslKeyStore();
 
-	public abstract HttpService getService();
-
+    public abstract HttpService getService();
 }
