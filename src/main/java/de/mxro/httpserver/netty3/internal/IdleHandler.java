c1c8569a28bbc3884aa48f7299450efd7b4a8e6e
mxro <mxro@nowhere.com>
2014-10-15 20:22:12 +1100

    a

---
+++ src/main/java/de/mxro/httpserver/netty3/internal/IdleHandler.java
@@ -0,0 +1,17 @@
+package de.mxro.httpserver.netty3.internal;
+
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.handler.timeout.IdleState;
+import org.jboss.netty.handler.timeout.IdleStateAwareChannelHandler;
+import org.jboss.netty.handler.timeout.IdleStateEvent;
+
+public class IdleHandler extends IdleStateAwareChannelHandler {
+    @Override
+    public void channelIdle(final ChannelHandlerContext ctx, final IdleStateEvent e) {
+        if (e.getState() == IdleState.READER_IDLE) {
+            e.getChannel().close();
+        } else if (e.getState() == IdleState.WRITER_IDLE) {
+            e.getChannel().write("dummy");
+        }
+    }
+}
