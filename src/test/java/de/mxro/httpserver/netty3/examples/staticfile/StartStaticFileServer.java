1a48735ae5961f5e8711f2e383349875d00deb65
mxro <mxro@nowhere.com>
2014-10-16 11:02:10 +1100

    Fixing shutdown server Autocommit Thu Oct 16 11:02:10 EST 2014

--- src/test/java/de/mxro/httpserver/netty3/examples/staticfile/StartStaticFileServer.java
+++ src/test/java/de/mxro/httpserver/netty3/examples/staticfile/StartStaticFileServer.java
@@ -1,7 +1,46 @@
 package de.mxro.httpserver.netty3.examples.staticfile;
 
+import de.mxro.async.Async;
+import de.mxro.async.Deferred;
+import de.mxro.async.callbacks.SimpleCallback;
+import de.mxro.async.callbacks.ValueCallback;
+import de.mxro.async.jre.AsyncJre;
+import de.mxro.fn.Success;
+import de.mxro.httpserver.HttpService;
+import de.mxro.httpserver.netty3.Netty3Server;
+import de.mxro.httpserver.netty3.Netty3ServerComponent;
+import de.mxro.httpserver.resources.ResourceProvider;
+import de.mxro.httpserver.resources.Resources;
+import de.mxro.httpserver.services.Services;
+import org.eclipse.xtext.xbase.lib.Exceptions;
+import org.eclipse.xtext.xbase.lib.InputOutput;
+
 @SuppressWarnings("all")
 public class StartStaticFileServer {
   public static void main(final String[] args) {
+    try {
+      ResourceProvider _fromClasspath = Resources.fromClasspath(StartStaticFileServer.class);
+      ResourceProvider _forWeb = Resources.forWeb(_fromClasspath);
+      final ResourceProvider source = Resources.cache(_forWeb);
+      final HttpService service = Services.resources(source);
+      final Deferred<Netty3ServerComponent> _function = new Deferred<Netty3ServerComponent>() {
+        public void get(final ValueCallback<Netty3ServerComponent> cb) {
+          Netty3Server.start(service, 8081, cb);
+        }
+      };
+      final Netty3ServerComponent server = AsyncJre.<Netty3ServerComponent>waitFor(_function);
+      InputOutput.<String>println("Download file from at http://localhost:8081/bigfile.js");
+      InputOutput.<String>println("Press key to stop server");
+      System.in.read();
+      final Deferred<Success> _function_1 = new Deferred<Success>() {
+        public void get(final ValueCallback<Success> cb) {
+          SimpleCallback _wrap = Async.wrap(cb);
+          server.stop(_wrap);
+        }
+      };
+      AsyncJre.<Success>waitFor(_function_1);
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
   }
 }
--- src/test/xtend/de/mxro/httpserver/netty3/examples/staticfile/StartStaticFileServer.xtend
+++ src/test/xtend/de/mxro/httpserver/netty3/examples/staticfile/StartStaticFileServer.xtend
@@ -1,7 +1,25 @@
 package de.mxro.httpserver.netty3.examples.staticfile
 
+import de.mxro.async.Async
+import de.mxro.async.jre.AsyncJre
+import de.mxro.httpserver.netty3.Netty3Server
+import de.mxro.httpserver.resources.Resources
+import de.mxro.httpserver.services.Services
+
 class StartStaticFileServer {
 	def static void main(String[] args) {
 		
+		val source = Resources.cache(Resources.forWeb(Resources.fromClasspath(StartStaticFileServer)));
+		
+		val service = Services.resources(source)
+		
+
+		val server = AsyncJre.waitFor([cb | Netty3Server.start(service, 8081, cb) ])
+		 
+		 println("Download file from at http://localhost:8081/bigfile.js")
+		 println('Press key to stop server')
+		 System.in.read
+		
+		AsyncJre.waitFor([cb | server.stop(Async.wrap(cb))]);
 	}
 }
