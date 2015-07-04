90a7b5ca054bbef5faaf854624b23ef827c978cd
mxro <mxro@nowhere.com>
2015-07-05 09:23:38 +1000

    Moving projects to Java Delight Autocommit Sun Jul 05 09:23:38 EST 2015

---
+++ src/test/src/main/java/de/mxro/httpserver/netty3/examples/post/StartPostServer.java
@@ -0,0 +1,151 @@
+package de.mxro.httpserver.netty3.examples.post;
+
+import de.mxro.fn.Success;
+import de.mxro.httpserver.HttpService;
+import de.mxro.httpserver.netty3.Netty3Server;
+import de.mxro.httpserver.netty3.Netty3ServerComponent;
+import de.mxro.httpserver.services.Services;
+import delight.async.AsyncCommon;
+import delight.async.Operation;
+import delight.async.callbacks.SimpleCallback;
+import delight.async.callbacks.ValueCallback;
+import delight.async.jre.Async;
+import java.util.HashMap;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.xbase.lib.Exceptions;
+import org.eclipse.xtext.xbase.lib.Functions.Function0;
+import org.eclipse.xtext.xbase.lib.InputOutput;
+
+@SuppressWarnings("all")
+public class StartPostServer {
+  public static void main(final String[] args) {
+    try {
+      final HashMap<String, HttpService> services = new HashMap<String, HttpService>();
+      HttpService _echo = Services.echo();
+      services.put("/service", _echo);
+      byte[] _bytes = StartPostServer.PAGE.getBytes();
+      HttpService _data = Services.data(_bytes, "text/html");
+      services.put("*", _data);
+      final Operation<Netty3ServerComponent> _function = new Operation<Netty3ServerComponent>() {
+        @Override
+        public void apply(final ValueCallback<Netty3ServerComponent> cb) {
+          HttpService _dispatcher = Services.dispatcher(services);
+          Netty3Server.start(_dispatcher, 8081, cb);
+        }
+      };
+      final Netty3ServerComponent server = Async.<Netty3ServerComponent>waitFor(_function);
+      InputOutput.<String>println("Open page at http://localhost:8081");
+      InputOutput.<String>println("Press key to stop server");
+      System.in.read();
+      final Operation<Success> _function_1 = new Operation<Success>() {
+        @Override
+        public void apply(final ValueCallback<Success> cb) {
+          SimpleCallback _wrap = AsyncCommon.wrap(cb);
+          server.stop(_wrap);
+        }
+      };
+      Async.<Success>waitFor(_function_1);
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+  
+  private final static String PAGE = new Function0<String>() {
+    public String apply() {
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.newLine();
+      _builder.append("<html>");
+      _builder.newLine();
+      _builder.newLine();
+      _builder.append("<body>");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js\"></script>");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("<script>");
+      _builder.newLine();
+      _builder.append("\t\t");
+      _builder.append("var text = \"\";");
+      _builder.newLine();
+      _builder.append("\t\t");
+      _builder.append("for (var i=0;i<20;i++) {");
+      _builder.newLine();
+      _builder.append("\t\t\t");
+      _builder.append("text += \"\"+Math.random();");
+      _builder.newLine();
+      _builder.append("\t\t");
+      _builder.append("}");
+      _builder.newLine();
+      _builder.append("\t\t");
+      _builder.append("setInterval(function() {");
+      _builder.newLine();
+      _builder.append("\t\t ");
+      _builder.append("$.ajax({");
+      _builder.newLine();
+      _builder.append("\t\t \t        ");
+      _builder.append("type: \"POST\",");
+      _builder.newLine();
+      _builder.append("                    ");
+      _builder.append("url: \"/service\",");
+      _builder.newLine();
+      _builder.append("                    ");
+      _builder.append("data:  \"Some big chunk of data follows: \"+text,");
+      _builder.newLine();
+      _builder.append("                    ");
+      _builder.append("processData: false,");
+      _builder.newLine();
+      _builder.append("                    ");
+      _builder.append("contentType: \'text\',");
+      _builder.newLine();
+      _builder.append("                    ");
+      _builder.append("dataType: \'text\',");
+      _builder.newLine();
+      _builder.append("                    ");
+      _builder.append("success: function(data, textStatus, rew) {");
+      _builder.newLine();
+      _builder.append("                        ");
+      _builder.append("$(\"body\").append(data);");
+      _builder.newLine();
+      _builder.append("                        ");
+      _builder.newLine();
+      _builder.newLine();
+      _builder.append("                       ");
+      _builder.newLine();
+      _builder.append("                    ");
+      _builder.append("}");
+      _builder.newLine();
+      _builder.append("                    ");
+      _builder.newLine();
+      _builder.append("                ");
+      _builder.append("});");
+      _builder.newLine();
+      _builder.append("                ");
+      _builder.append("}, 500);");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("</script>");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.newLine();
+      _builder.append("</body>");
+      _builder.newLine();
+      _builder.newLine();
+      _builder.newLine();
+      _builder.append("</html>");
+      _builder.newLine();
+      _builder.newLine();
+      return _builder.toString();
+    }
+  }.apply();
+}
---
+++ src/test/src/main/java/de/mxro/httpserver/netty3/examples/staticfile/StartStaticFileServer.java
@@ -0,0 +1,64 @@
+package de.mxro.httpserver.netty3.examples.staticfile;
+
+import de.mxro.fn.Success;
+import de.mxro.httpserver.HttpService;
+import de.mxro.httpserver.netty3.Netty3Server;
+import de.mxro.httpserver.netty3.Netty3ServerComponent;
+import de.mxro.httpserver.resources.ResourceProvider;
+import de.mxro.httpserver.resources.Resources;
+import de.mxro.httpserver.services.Services;
+import delight.async.AsyncCommon;
+import delight.async.Operation;
+import delight.async.callbacks.SimpleCallback;
+import delight.async.callbacks.ValueCallback;
+import delight.async.jre.Async;
+import org.eclipse.xtext.xbase.lib.Exceptions;
+import org.eclipse.xtext.xbase.lib.InputOutput;
+
+@SuppressWarnings("all")
+public class StartStaticFileServer {
+  public static void main(final String[] args) {
+    try {
+      ResourceProvider _fromClasspath = Resources.fromClasspath(StartStaticFileServer.class);
+      ResourceProvider _forWeb = Resources.forWeb(_fromClasspath);
+      final ResourceProvider source = Resources.cache(_forWeb);
+      final HttpService service = Services.resources(source);
+      final Operation<Success> _function = new Operation<Success>() {
+        @Override
+        public void apply(final ValueCallback<Success> cb) {
+          SimpleCallback _wrap = AsyncCommon.wrap(cb);
+          service.start(_wrap);
+        }
+      };
+      Async.<Success>waitFor(_function);
+      final Operation<Netty3ServerComponent> _function_1 = new Operation<Netty3ServerComponent>() {
+        @Override
+        public void apply(final ValueCallback<Netty3ServerComponent> cb) {
+          Netty3Server.start(service, 8081, cb);
+        }
+      };
+      final Netty3ServerComponent server = Async.<Netty3ServerComponent>waitFor(_function_1);
+      InputOutput.<String>println("Download file from at http://localhost:8081/bigfile.js");
+      InputOutput.<String>println("Press key to stop server");
+      System.in.read();
+      final Operation<Success> _function_2 = new Operation<Success>() {
+        @Override
+        public void apply(final ValueCallback<Success> cb) {
+          SimpleCallback _wrap = AsyncCommon.wrap(cb);
+          server.stop(_wrap);
+        }
+      };
+      Async.<Success>waitFor(_function_2);
+      final Operation<Success> _function_3 = new Operation<Success>() {
+        @Override
+        public void apply(final ValueCallback<Success> cb) {
+          SimpleCallback _wrap = AsyncCommon.wrap(cb);
+          service.stop(_wrap);
+        }
+      };
+      Async.<Success>waitFor(_function_3);
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+}
